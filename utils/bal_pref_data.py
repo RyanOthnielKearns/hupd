e05422dd503b8b55cbc3271505aeb24f03b453c9
Ryan Othniel Kearns <ryankearns25@gmail.com>
2022-03-08 14:41:44 -0800

    More fixes for VM testing

--- utils/bal_pref_data.py
+++ utils/bal_pref_data.py
@@ -13,48 +13,54 @@ parser.add_argument('--seed', type=int, default = 224, help = "Set the random se
 
 args = parser.parse_args()
 
+all_json_ids = set()
+for year in np.arange(2005, 2017 + 1, 1):
+	all_json_ids.add(set([ i.split(".")[0] for i in os.listdir(f"seldata{year}/") ]))
+
 sample_metadata = pd.read_feather(f"hupd_{args.ipc_code}_metadata_2022-03-04.feather")
 
 sample_metadata = sample_metadata.loc[
 	np.logical_or(
 		sample_metadata['decision'] == "ACCEPTED",
 		sample_metadata['decision'] == "REJECTED",
-	)
+	) & sample_metadata['application_number'] in all_json_ids
 ].assign(
-	decision_binary = lambda x: np.where(x['decision'] == ACCEPTED, 1, 0)
+	decision_binary = lambda x: np.where(x['decision'] == "ACCEPTED", 1, 0)
 )
 
 true_N = sample_metadata['decision_binary'].shape[0]
-# print(f"{true_N} actual accepted patents")
+print(f"{true_N} actual patents.")
 true_N_accepted = sample_metadata['decision_binary'].sum()
+print(f"  {true_N_accepted} accepted patents.")
 true_N_rejected=  true_N - true_N_accepted
+print(f"  {true_N_rejected} rejected patents.")
 true_data_prop = true_N_accepted / (true_N)
 
 
-desired_N_accepted = int(parser.bal_prop * true_N)
+desired_N_accepted = int(args.bal_prop * true_N)
 desired_N_rejected = true_N - desired_N_accepted
 
 # note if we don't have enough for true split
 if desired_N_rejected > true_N_rejected:
 	desired_N_rejected = true_N_rejected
-	desired_N_accepted = int(desired_N_rejected * (parser.bal_prop / (1 - parser.bal_prop)))
+	desired_N_accepted = int(desired_N_rejected * (args.bal_prop / (1 - args.bal_prop)))
 if desired_N_accepted > true_N_accepted:
 	desired_N_accepted = true_N_accepted
-	desired_N_rejected = int(desired_N_accepted * ((1 - parser.bal_prop) / parser.bal_prop))
+	desired_N_rejected = int(desired_N_accepted * ((1 - args.bal_prop) / args.bal_prop))
+
+print(f"Selecting {desired_N_accepted} accepted patents and {desired_N_rejected} rejected patents to fix balance of {args.bal_prop}")
 
 sample_metadata_acc = sample_metadata.loc[
 	sample_metadata['decision_binary'] == 1
-].reset_index().sample(n = desired_N_accepted, replace = False, random_state = parser.seed)
+].reset_index().sample(n = desired_N_accepted, replace = False, random_state = args.seed)
 
 sample_metadata_rej = sample_metadata.loc[
 	sample_metadata['decision_binary'] == 0
-].reset_index().sample(n = desired_N_rejected, replace = False, random_state = parser.seed)
+].reset_index().sample(n = desired_N_rejected, replace = False, random_state = args.seed)
 
 bal_sample_metadata = pd.concat((sample_metadata_acc, sample_metadata_rej), axis = 0)
 
 bal_sample_metadata.to_feather(f"hupd_{args.ipc_code}_metadata_2022-03-04_bal.feather")
-
-return # FIXME I'm adding this for debugging purposes until I can be sure it will move things as expected
 
 # construct balanced data
 bal_json_files = bal_sample_metadata['application_number'] + ".json"
@@ -65,10 +71,10 @@ year_range = np.arange(year_min, year_max + 1, 1)
 
 ## move files to balanced data folder
 
-if not os.path.exists('bal_data'):
-	os.mkdir('bal_data')
+# if not os.path.exists('bal_data'):
+# 	os.mkdir('bal_data')
 
-for year in year_range:
-	files_in_balanced_sample = set(os.listdir(f"seldata{year}/")).intersection(set(bal_json_files))
-	for jf in files_in_balanced_sample:
-		shutil.move(f"selfdata{year}/" + jf, "bal_data/" + jf)
+# for year in year_range:
+# 	files_in_balanced_sample = set(os.listdir(f"seldata{year}/")).intersection(set(bal_json_files))
+# 	for jf in files_in_balanced_sample:
+# 		shutil.move(f"seldata{year}/" + jf, "bal_data/" + jf)
