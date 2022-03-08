d6abc58c0e604bb044401f8fcd42c35978243bb9
RyanOthnielKearns <ryankearns25@gmail.com>
2022-03-08 20:24:29 +0000

    Debugging a bit

--- utils/bal_pref_data.py
+++ utils/bal_pref_data.py
@@ -11,6 +11,8 @@ parser.add_argument('--ipc_code', type=str, default = "G06F1730", help="IPC Code
 parser.add_argument('--bal_prop', type=float, default = 0.50, help="Balance proportion (out of 1): higher number --> more accepted.")
 parser.add_argument('--seed', type=int, default = 224, help = "Set the random seed for split reproducibility.")
 
+args = parser.parse_args()
+
 sample_metadata = pd.read_feather(f"hupd_{args.ipc_code}_metadata_2022-03-04.feather")
 
 sample_metadata = sample_metadata.loc[
@@ -23,6 +25,7 @@ sample_metadata = sample_metadata.loc[
 )
 
 true_N = sample_metadata['decision_binary'].shape[0]
+# print(f"{true_N} actual accepted patents")
 true_N_accepted = sample_metadata['decision_binary'].sum()
 true_N_rejected=  true_N - true_N_accepted
 true_data_prop = true_N_accepted / (true_N)
@@ -35,7 +38,7 @@ desired_N_rejected = true_N - desired_N_accepted
 if desired_N_rejected > true_N_rejected:
 	desired_N_rejected = true_N_rejected
 	desired_N_accepted = int(desired_N_rejected * (parser.bal_prop / (1 - parser.bal_prop)))
-else if desired_N_accepted > true_N_accepted: 
+if desired_N_accepted > true_N_accepted:
 	desired_N_accepted = true_N_accepted
 	desired_N_rejected = int(desired_N_accepted * ((1 - parser.bal_prop) / parser.bal_prop))
 
@@ -51,6 +54,8 @@ bal_sample_metadata = pd.concat((sample_metadata_acc, sample_metadata_rej), axis
 
 bal_sample_metadata.to_feather(f"hupd_{args.ipc_code}_metadata_2022-03-04_bal.feather")
 
+return # FIXME I'm adding this for debugging purposes until I can be sure it will move things as expected
+
 # construct balanced data
 bal_json_files = bal_sample_metadata['application_number'] + ".json"
 
@@ -64,12 +69,6 @@ if not os.path.exists('bal_data'):
 	os.mkdir('bal_data')
 
 for year in year_range:
-	files_in_balanced_sample = set(os.listdir(f"{year}/")).intersection(set(bal_json_files))
+	files_in_balanced_sample = set(os.listdir(f"seldata{year}/")).intersection(set(bal_json_files))
 	for jf in files_in_balanced_sample:
-		shutil.move(f"{year}/" + jf, "bal_data/" + jf)
-
-
-
-
-
-
+		shutil.move(f"selfdata{year}/" + jf, "bal_data/" + jf)
